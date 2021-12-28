a122edccf8f5f157475c84db3db7331f4f2daebc
BenZeltser <92685838+BenZeltser@users.noreply.github.com>
2021-12-28 20:57:31 +0200

    Excellent Progress

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -4,23 +4,29 @@ import random
 from src.GraphInterface import GraphInterface
 from src.GrapthAlgoInterface import GraphAlgoInterface
 from src.DiGraph import DiGraph
+from src.Heap import Heap, dijkstra
+
 '''Graph Algo class implements GraphAlgoInterface'''
 
 class GraphAlgo(GraphAlgoInterface):
 
     '''Constructor'''
+
     def __init__(self, myGraph):
         myGraph = DiGraph()
         self.myGraph = myGraph
         '''New Graph'''
 
+    '''Get'''
+
     def get_graph(self) -> GraphInterface:
         return self.myGraph
 
     '''Load'''
+
     def load_from_json(self, file_name: str) -> bool:
         try:
-            '''Open file'''
+            '''Open file as read'''
             with open(file_name, 'r') as f:
                 load = json.load(f)
                 '''Init graph instance'''
@@ -42,8 +48,52 @@ class GraphAlgo(GraphAlgoInterface):
             raise NotImplementedError
         return True
 
-    '''save'''
+    '''Save'''
+
     def save_to_json(self, file_name: str) -> bool:
-        pass
+        try:
+
+            '''Create Dict parameters (dynamic ds)'''
+            jdict = {'Edges': [], 'Nodes': []}
+
+            '''Open (create) Json file as write'''
+            with open(file_name, 'w') as f:
+
+                '''Iterate in a nested loop. \n
+                   Append edges.'''
+
+                for i in self.get_graph().get_all_v().keys():
+                    for j,k in self.get_graph().all_in_edges_of_node(i).items():
+                        jdict['Edges'].append(
+                            {'src': i, 'dst': j, 'w': k})
+
+
+                '''append nodes'''
+
+                for i in self.get_graph().get_all_v().values():
+                    jdict['Nodes'].append(
+                        {'id':i.key}
+                    )
+
+                '''Apply the data to the file'''
+                size = len(jdict)
+                out = json.dumps(jdict, allow_nan=True, indent=size)
+                f.write(out)
+
+                '''-> Condit'''
+                return True
+
+        except:
+            raise NotImplementedError
+
 
     '''Algorithms'''
+
+
+    '''Shortest path - Implementation idea inspired by Wlliam Fiset '''
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        myGraph = self.myGraph
+        return dijkstra(myGraph,id1,id2)
+
+
