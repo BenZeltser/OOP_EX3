abead5d20ba0a281769347a784773f5b6e3886e8
BenZeltser <92685838+BenZeltser@users.noreply.github.com>
2021-12-28 23:14:37 +0200

    TSP Done

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,5 +1,11 @@
+import itertools
 import json
 import random
+from typing import List
+from sys import maxsize
+from itertools import permutations
+
+import length as length
 
 from src.GraphInterface import GraphInterface
 from src.GrapthAlgoInterface import GraphAlgoInterface
@@ -95,5 +101,40 @@ class GraphAlgo(GraphAlgoInterface):
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         myGraph = self.myGraph
         return dijkstra(myGraph,id1,id2)
+
+    '''TSP'''
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        myGraph = self.myGraph
+        size = len(node_lst)
+        vertex = []
+        i=0
+        while(i<size):
+                vertex.append(i)
+        min_path = maxsize
+        next_permutation = permutations(vertex)
+        for i in next_permutation:
+            current_pathweight = 0
+            k = size
+            for j in i:
+                current_pathweight += myGraph[k][j]
+                k = j
+            current_pathweight += myGraph[k][size]
+            min_path = min(min_path, current_pathweight)
+        return min_path
+
+'''    def solve_tsp_dynamic(points):
+        all_distances = [[length(x, y) for y in points] for x in points]
+        A = {(frozenset([0, idx + 1]), idx + 1): (dist, [0, idx + 1]) for idx, dist in enumerate(all_distances[0][1:])}
+        cnt = len(points)
+        for m in range(2, cnt):
+            B = {}
+            for S in [frozenset(C) | {0} for C in itertools.combinations(range(1, cnt), m)]:
+                for j in S - {0}:
+                    B[(S, j)] = min([(A[(S - {j}, k)][0] + all_distances[k][j], A[(S - {j}, k)][1] + [j]) for k in S if
+                                     k != 0 and k != j])
+            A = B
+        res = min([(A[d][0] + all_distances[0][d[1]], A[d][1]) for d in iter(A)])
+        return res[1]'''
 
 
